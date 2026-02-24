6e07efc37d5d9e9e5cf1ce45028ebadad87a54a7
Ivanna Malashchuk <imalaschuk@gmail.com>
2026-02-25 01:45:57 +0200

    add lab3

---
+++ MyFisrtUIKitApp/PostTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostTableViewCell.swift
+//  MyFisrtUIKitApp
+//
+//  Created by Ivanna Malashchuk on 14.02.2026.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MyFisrtUIKitApp/TestClass.swift
@@ -0,0 +1,20 @@
+//
+//  TestClass.swift
+//  MyFisrtUIKitApp
+//
+//  Created by Ivanna Malashchuk on 19.02.2026.
+//
+
+import UIKit
+
+class TestClass: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
